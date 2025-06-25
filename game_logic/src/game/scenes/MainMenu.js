194b404e689b6a83d7cb571538a9a7812ad40338
Edwin González <130111056+edslogia@users.noreply.github.com>
2025-06-25 01:16:56 +0000

    refactor MainMenu scene: update text styling and position, improve code formatting

--- game_logic/src/game/scenes/MainMenu.js
+++ game_logic/src/game/scenes/MainMenu.js
@@ -1,33 +1,26 @@
 import { EventBus } from '../EventBus';
 import { Scene } from 'phaser';
+import { FONT_VT323 } from '../config/fonts';
 
-export class MainMenu extends Scene
-{
+export class MainMenu extends Scene {
     logoTween;
 
-    constructor ()
-    {
+    constructor() {
         super('MainMenu');
     }
 
-    create ()
-    {
+    create() {
         // Agrega la imagen de fondo y la ajusta al tamaño de la escena
         const bg = this.add.image(0, 0, 'background').setOrigin(0);
         bg.displayWidth = this.sys.game.config.width;
         bg.displayHeight = this.sys.game.config.height;
 
-        this.add.text(512, 700, '4geeks', {
-            fontFamily: 'Arial Black', fontSize: 38, color: '#ffffff',
-            stroke: '#000000', strokeThickness: 8,
-            align: 'center'
-        }).setDepth(100).setOrigin(0.5);
-        
+        this.add.text(512, 500, '4Geeks', FONT_VT323).setDepth(100).setOrigin(0.5);
+
         EventBus.emit('current-scene-ready', this);
     }
 
-    changeScene ()
-    {
+    changeScene() {
         this.scene.start('Game');
     }
 
