5ce2ac121758b7d828de30c8a4e07bd11900b687
Edwin González <130111056+edslogia@users.noreply.github.com>
2025-06-26 17:10:51 +0000

    se ajusta el tamaño de visualización del zombie a 40x40

--- game_logic/src/game/objects/zombieObject.js
+++ game_logic/src/game/objects/zombieObject.js
@@ -11,7 +11,7 @@ export class ZombieObject {
         const col = Phaser.Math.Between(0, cols - 1);
         const zombieX = col * colWidth + colWidth / 2;
         const zombie = this.scene.physics.add.image(zombieX, zombieY, 'zombie')
-            .setDisplaySize(50, 50)
+            .setDisplaySize(40, 40)
             .setCollideWorldBounds(true);
         zombie.body.setAllowGravity(false);
         zombie.setData('health', 15);
