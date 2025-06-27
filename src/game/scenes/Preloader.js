b0096af1d52b3a35de8653a4e4a887636b0eaaea
Edwin González <130111056+edslogia@users.noreply.github.com>
2025-06-27 17:50:06 +0000

    se ajusta tamaño de imagen en preload, se sube barra de carga

--- src/game/scenes/Preloader.js
+++ src/game/scenes/Preloader.js
@@ -9,15 +9,14 @@ export class Preloader extends Scene {
 
     init() {
         //  We loaded this image in our Boot Scene, so we can display it here
-        this.add.image(512, 384, 'background');
-
+        this.add.image(0, 0, 'background').setOrigin(0).setDisplaySize(768, 576);
 
 
         //  A simple progress bar. This is the outline of the bar.
-        this.add.rectangle(512, 384, 468, 32).setStrokeStyle(1, 0xffffff);
+        this.add.rectangle(384, 60, 468, 32).setStrokeStyle(1, 0xffffff);
 
         //  This is the progress bar itself. It will increase in size from the left based on the % of progress.
-        const bar = this.add.rectangle(512 - 230, 384, 4, 28, 0xffffff);
+        const bar = this.add.rectangle(384 - 230, 60, 4, 28, 0xffffff);
 
         //  Use the 'progress' event emitted by the LoaderPlugin to update the loading bar
         this.load.on('progress', (progress) => {
