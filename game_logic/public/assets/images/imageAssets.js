b7bc15125759a7655d3f1b11d9f07bb5d5400e07
Edwin González <130111056+edslogia@users.noreply.github.com>
2025-06-27 15:24:39 +0000

    se agrega imagen para fondo de escena

--- game_logic/public/assets/images/imageAssets.js
+++ game_logic/public/assets/images/imageAssets.js
@@ -8,4 +8,5 @@ export const imageAssets = [
     { key: 'spark', path: 'images/spark.png' },
     { key: 'blood', path: 'images/blood.png' },
     { key: 'explosion_fire', path: 'images/explosion_fire.png' },
+    { key: 'arena1', path: 'images/arena_1.png' },
 ];
